d809e4252397896c8ee3c24b05de08375a952954
Bruno Passos <brpassos.programador@gmail.com>
2022-12-18 12:29:16 -0300

    rest template

--- locacao/src/main/java/br/edu/infnet/locacao/LocacaoApplication.java
+++ locacao/src/main/java/br/edu/infnet/locacao/LocacaoApplication.java
@@ -2,9 +2,16 @@ package br.edu.infnet.locacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LocacaoApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LocacaoApplication.class, args);
--- locacao/src/main/java/br/edu/infnet/locacao/resources/LocacaoResource.java
+++ locacao/src/main/java/br/edu/infnet/locacao/resources/LocacaoResource.java
@@ -1,18 +1,28 @@
 package br.edu.infnet.locacao.resources;
 
+import br.edu.infnet.locacao.resources.dto.ClienteDTO;
 import br.edu.infnet.locacao.resources.dto.LocacaoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/locacoes")
 public class LocacaoResource {
 
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @PostMapping
     public void efetuaLocacao(@RequestBody LocacaoDTO locacaoDTO) {
+
+        ClienteDTO clienteDTO = restTemplate.getForObject("http://localhost:8282/clientes/"+locacaoDTO.getClienteId(), ClienteDTO.class);
+
+        System.out.println(clienteDTO);
         System.out.println(locacaoDTO);
     }
 }
